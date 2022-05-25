32abfa328239c9a9a2e99bc4a354ce2220b6521a
shanto <santo15-10959@diu.edu.bd>
2022-05-25 21:26:12 +0600

    get single tool

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,25 +16,43 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
-        await client.connect();
+async function run() {
+  try {
+    await client.connect();
 
-        const toolsCollection = client.db('colors_paint').collection('tools');
+    const toolsCollection = client.db('colors_paint').collection('tools');
 
-        app.get('/tool', async(req, res) => {
-            const query = {};
-            const cursor = toolsCollection.find(query);
-            const tools = await cursor.toArray();
-            res.send(tools);
+    const reviewCollection = client.db('colors_paint').collection('reviews');
 
-        })
+    // get all tools
+    app.get('/tool', async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
 
-    }
+    // get a tool with id
+    app.get('/tool/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query)
+      res.send(tool);
+    });
 
-    finally{
+    // post a review
+    app.post('/review', async(req,res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
 
-    }
+    })
+
+  }
+
+  finally {
+
+  }
 
 }
 run().catch(console.dir);
