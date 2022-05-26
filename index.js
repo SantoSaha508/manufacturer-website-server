fc65df301c1f79b98a978793293718972df4fd52
shanto <santo15-10959@diu.edu.bd>
2022-05-26 18:37:24 +0600

    get admin role

--- index.js
+++ index.js
@@ -46,21 +46,37 @@ async function run() {
     const orderCollection = client.db('colors_paint').collection('orders');
 
     // get all users
-    app.get('/user', varifyJWT,  async(req, res) => {
+    app.get('/user', varifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users)
+    });
+
+    // get use to check admin
+    app.get('/admin/:email', async(req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin});
     })
 
     // make admin
     app.put('/user/admin/:email', varifyJWT, async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: {role: 'admin'},
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      
-      res.send(result);
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'forbidden'})
+      }
+
     })
 
     // store user *
@@ -125,8 +141,8 @@ async function run() {
         const orders = await orderCollection.find(query).toArray();
         return res.send(orders);
       }
-      else{
-        return res.status(403).send({message: 'Forbidden access'});
+      else {
+        return res.status(403).send({ message: 'Forbidden access' });
       }
 
     })
